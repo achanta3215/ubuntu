b6eff7a5ca9a4a0585055b730564356e783d215c
Krishna Sameer Achanta <achanta3215@gmail.com>
2020-08-10 20:21:15 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# https://sourcedigit.com/24976-vim-8-2-released-how-to-install-vim-in-ubuntu-linux/
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt update
+sudo apt install vim
+
+# To uninstall
+sudo apt install ppa-purge
+sudo ppa-purge ppa:jonathonf/vim
+sudo add-apt-repository --remove ppa:jonathonf/vim
